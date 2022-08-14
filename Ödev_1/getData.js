d0b4b6487e9ff837d040b0252c8f3a4fdbcc3a3c
Alihan Degirmenciler <alihandegirmenciler@gmail.com>
2022-08-14 20:58:21 +0300

    odev 1 yapıldı

--- Ödev_1/getData.js
+++ Ödev_1/getData.js
@@ -1,21 +1,12 @@
 import axios from "axios";
 
-// const getData = async (user_id) => {
-
-//     const {data: User} = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`);
-//     const {data: Posts} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
-
-//     User.Posts = Posts;
-
-//     return User;
-// };
 
 async function getData(user_id) {
 
     const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`);
     const { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
 
-    user.posts = posts
+    user.posts = posts;
     return user;
 };
 
