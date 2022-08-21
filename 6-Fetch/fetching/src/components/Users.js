02b2b4d25117da4deca485019857e0fe0c3a1348
Alihan Degirmenciler <alihandegirmenciler@gmail.com>
2022-08-21 13:34:57 +0300

    fetching, axios done

---
+++ 6-Fetch/fetching/src/components/Users.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from "axios"
+
+function Users() {
+
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);   // loading yazısı
+
+    useEffect(() => {
+        axios("https://jsonplaceholder.typicode.com/users")     // Component ilk yüklendiği zaman sadece 1 kere fetch işlemi yapmak için useEffect kullandık
+            .then((res) => { setUsers(res.data) })
+            .catch((e) => console.log(e))
+            .finally(() => setIsLoading(false));              // burada veriler gelince loading yazısını false yapıyoruz
+    }, [])
+
+    return (
+        <div>
+            <h1>Users</h1>
+            {isLoading && <div>Loading...</div>}
+            {users.map((user) => <div key={user.id}>{user.name}</div>)}
+        </div>
+    )
+}
+
+export default Users
