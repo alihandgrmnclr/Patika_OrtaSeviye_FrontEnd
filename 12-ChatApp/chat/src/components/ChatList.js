bcd54e02c5117e3ff977cd0c97c07270f04ac49b
Alihan Degirmenciler <alihandegirmenciler@gmail.com>
2022-08-31 23:16:26 +0300

    chat app

---
+++ 12-ChatApp/chat/src/components/ChatList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from "./styles.module.css"
+import { useChat } from "../context/ChatContext"
+import ChatItem from "./ChatItem"
+
+function ChatList() {
+
+
+  const { messages } = useChat();
+
+  return (
+    <div className={styles.chatList}>
+      <div>
+        {
+          messages.map((item, key) => (
+            <ChatItem key={key} item={item} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default ChatList
